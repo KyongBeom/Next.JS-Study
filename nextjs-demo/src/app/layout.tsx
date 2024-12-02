d983c01cc90a48f2db45a9c3025c9eccfdfabc77
김용범 <yongbeom@yongbeomMacBookAir.local>
2024-12-02 17:44:15 +0900

    nextJs Routing

--- nextjs-demo/src/app/layout.tsx
+++ nextjs-demo/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { Navigation } from "./components/navigation";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -29,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header className="bg-slate-900 text-white p-4 text-center">
-          <p>Welcome to NExt,js 15 Crash Course</p>
+          {/* <p>Welcome to NExt,js 15 Crash Course</p> */}
+          <Navigation/>
         </header>
         {children}
         <footer className="bg-slate-900 text-white p-4 text-center">
